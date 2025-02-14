07a7d719d61c08a4351b9b6451e8f205b683e7a8
realrains <real.longrain@gmail.com>
2025-02-15 01:09:10 +0900

    config(nvim): add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("realrain.config.lazy")
+require("realrain.core.options")
+require("realrain.core.keymaps")
+require("realrain.core.colorscheme")
---
+++ nvim/lua/realrain/core/keymaps.lua
@@ -0,0 +1,27 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+-- general keymaps
+
+keymap.set("i", "jk", "<ESC>") -- go out insert mode by 'jk'
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear search highlight
+keymap.set("n", "x", '"_x') -- x not going to copy deleted character to register
+keymap.set("n", "<leader>+", "<C-a>") -- increment number
+keymap.set("n", "<leader>-", "<C-x>") -- decrement number
+
+keymap.set("n", "<leader>sv", "<C-w>v") -- split winddow vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split winddow horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+
+-- plugin keymaps
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- maximize / restore splited window
+
